5636cf044152c94109e1124bf0ffb392bef68719
estelled <ms.estelle.dumas@gmail.com>
2018-10-09 19:08:05 +0200

    backup of E's work

---
+++ heatmap.js
@@ -0,0 +1,11 @@
+  var dots_array : circle[]= [364];
+    for (i=0; i<365; i++) {
+        dots_array[i]="i";
+    }
+    Array(5).fill(2);
+
+
+    function filter_name() {
+    document.getElementById("annotator").innerHTML = "Arnaud Rachez";
+    alert("filter_name function clicked")
+    }
